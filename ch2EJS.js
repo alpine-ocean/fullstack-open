3617c38ff73c90e36704e7a0d049a9066fd4152c
coreyj <9023272+alpine-ocean@users.noreply.github.com>
2026-07-24 02:01:04 +0000

    Complete exercises in 1b
    Exercises completed. Also 2 practice js files have been added that I
    have been using to mess around and do exercises from EloquentJS.

---
+++ ch2EJS.js
@@ -0,0 +1,13 @@
+
+let pound1 = '#'
+for (let i = 0; i < 8; i++) {
+    console.log(pound1)
+    pound1 += '#'
+}
+
+let i = 0
+while (i<7) {
+    console.log(pound1)
+    pound1 += '#'
+    i += 1
+}
---
+++ some-JS.js
@@ -0,0 +1,33 @@
+const object1 = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+}
+
+const object2 = {
+    name: 'Full Stack web application',
+    level: 'intermediate studies',
+    size: 5,
+}
+
+const object3 = {
+    name: {
+        first: 'Dan',
+        last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Stanford University',
+}
+
+console.log(object3.name.first)
+console.log(object1['age'])
+
+object1.address = 'Helsinki'
+object1['Secret Number'] = 12341
+
+console.log(object1)
+
+const square = p => p * p
+
+console.log(square(2))
+
